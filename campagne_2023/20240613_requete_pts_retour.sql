57bd2710489ea0172a2a446b9c9b44fcae8968d2
melquiad <laurent.haugomat@gmail.com>
2024-08-23 08:25:50 +0200

    sauvegarde du 23/08/2024

--- campagne_2023/20240613_requete_pts_retour.sql
+++ campagne_2023/20240613_requete_pts_retour.sql
@@ -1,22 +1,18 @@
 
 SET enable_nestloop = FALSE;
 
-SELECT v.annee, ep.npp, ep.leve
+----- ma version --------------------------------------------------------
+SELECT ep.incref + 10 AS annee, count(*) AS nb_pts_foret_prod_retrouves
 FROM v_liste_points_lt2 v
-INNER JOIN inv_exp_nm.e2point ep USING (npp)
-WHERE v.annee = 2023
-AND ep.leve = '1'
-AND ep.us_nm IN ('1', '5');
-
-SELECT ep.incref + 5, count(*)
-FROM v_liste_points_lt2 v
+INNER JOIN inv_prod_new.point_m2 pm USING (id_ech, id_point)
 INNER JOIN inv_exp_nm.e2point ep USING (npp)
 WHERE v.annee between 2010 AND 2023
 AND ep.leve = '1'
-AND ep.us_nm IN ('1', '5')
+AND ep.us_nm IN ('1', '5') AND pointok5 != '0'
 GROUP BY incref
 ORDER BY incref DESC;
 
+----- version Cédric --------------------------------------------------------------------------------------------------------------
 SELECT c.millesime AS annee
 , count(*) AS nb_pts_tires
 , count(*) FILTER (WHERE COALESCE(pointok5, '0') != '0') AS nb_pts_retrouves
@@ -32,10 +28,10 @@ AND e.type_ech = 'IFN'::bpchar
 AND e.ech_parent IS NOT NULL
 AND millesime BETWEEN 2010 AND 2023
 AND EXISTS (
-    SELECT p.id_point
-    FROM point_lt pl2
-    WHERE pl2.id_point = pl.id_point
-    AND pl2.id_ech < pl.id_ech
-)
+		SELECT p.id_point  --> pour exclure les points lt1pi2 du nb de points tirés, ça ne change rien pour les autres colonnes
+		FROM point_lt pl2
+		WHERE pl2.id_point = pl.id_point
+		AND pl2.id_ech < pl.id_ech
+			)
 GROUP BY annee
 ORDER BY annee DESC;
